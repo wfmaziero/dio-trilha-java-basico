b1eb1360b2156d2a90faf405643c717465311098
wfmaziero <wfmaziero@gmail.com>
2024-08-13 13:26:17 -0300

    Projeto novo SmartTv - aulas

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+        
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+
+
+        System.out.println("TV Ligada ?" + smartTv.ligada);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+    }
+}
---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+       
+        System.out.println(nomeCompleto);
+
+    }
+
+
+
+}
