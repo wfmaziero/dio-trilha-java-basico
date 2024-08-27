f28b418dd6ac3acc1d007e814b7b20b49b8f175a
wfmaziero <wfmaziero@gmail.com>
2024-08-27 11:29:44 -0300

    Continuando estudos em codigo fonte

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,10 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+       int nota =5;
+
+       String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+
+
+       System.out.println(resultado);
+    }
+}
---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,14 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "A";
+
+        if(sigla == "P")
+           System.out.println("PEQUENO");
+        else if(sigla == "M")
+           System.out.println("MEDIO");
+        else if(sigla == "G")
+           System.out.println("GRANDE");
+        else
+           System.out.println("INDEFINIDO");
+    }
+}
